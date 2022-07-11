e11a9ea01e6a7107b9b84458efa696c80ee7e443
a8120096 <103491502+a8120096@users.noreply.github.com>
2022-07-11 21:30:40 +0900

    Add files via upload

---
+++ bulletin-board.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <?php
+    // DB接続設定
+    $dsn = ʼデータベース名ʼ;
+    $user = ʼユーザー名ʼ;
+    $password = ʼパスワードʼ;
+    $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+    // テーブルの削除
+    // $sql = 'DROP TABLE tbbbs';
+    // $stmt = $pdo->query($sql);
+
+    // テーブルtbtestの作成
+    $sql = "CREATE TABLE IF NOT EXISTS tbbbs"
+        . " ("
+        . "id INT AUTO_INCREMENT PRIMARY KEY,"
+        . "name char(32),"
+        . "comment TEXT,"
+        . "passward TEXT,"
+        . "created_at datetime"
+        . ");";
+    $stmt = $pdo->query($sql);
+
+    // テーブルtbtestの中身の確認
+    $sql = 'SHOW CREATE TABLE tbbbs';
+    $result = $pdo->query($sql);
+    foreach ($result as $row) {
+        // echo $row[1];
+    }
+
+    if (isset($_POST["name"]) && isset($_POST["comment"]) && isset($_POST["passward"])) {
+        // 編集の場合
+        if ((!empty($_POST["name"])) && (!empty($_POST["comment"])) && (!empty($_POST["passward"])) && (!empty($_POST["id"]))) {
+            $id = $_POST["id"];
+            $name = $_POST["name"];
+            $comment = $_POST["comment"];
+            $passward = $_POST["passward"];
+            $created_at = date("Y-m-d H:i:s");
+            $sql = 'UPDATE tbbbs SET name=:name,comment=:comment,passward=:passward,created_at=:created_at WHERE id=:id';
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
+            $stmt->bindParam(':passward', $passward, PDO::PARAM_STR);
+            $stmt->bindParam(':created_at', $created_at, PDO::PARAM_STR);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+        }
+
+        // 新規投稿の場合
+        if ((!empty($_POST["name"])) && (!empty($_POST["comment"])) && (!empty($_POST["passward"]) && (empty($_POST["id"])))) {
+            // テーブルtbtestにレコードを追加
+            $sql = $pdo->prepare("INSERT INTO tbbbs (name, comment, passward, created_at) VALUES (:name, :comment, :passward, :created_at)");
+            $sql->bindParam(':name', $name, PDO::PARAM_STR);
+            $sql->bindParam(':comment', $comment, PDO::PARAM_STR);
+            $sql->bindParam(':passward', $passward, PDO::PARAM_STR);
+            $sql->bindParam(':created_at', $created_at, PDO::PARAM_STR);
+            $name = $_POST["name"];
+            $comment = $_POST["comment"];
+            $passward = $_POST["passward"];
+            $created_at = date("Y-m-d H:i:s");
+            $sql->execute();
+        }
+    }
+
+    // 記事削除処理
+    if (isset($_POST["del_num"]) && isset($_POST["del_passward"])) {
+        if ((!empty($_POST["del_num"])) && (!empty($_POST["del_passward"]))) {
+            // フォームから値の読み込み
+            $del_num = $_POST["del_num"];
+            $del_passward = $_POST["del_passward"];
+
+            // idが$del_numのパスワードを抽出
+            $sql = 'SELECT * FROM tbbbs WHERE id=:del_num';
+            $stmt = $pdo->prepare($sql);                  // ←差し替えるパラメータを含めて記述したSQLを準備し、
+            $stmt->bindParam(':del_num', $del_num, PDO::PARAM_INT); // ←その差し替えるパラメータの値を指定してから、
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            foreach ($results as $row) {
+                $passward =  $row['passward'];
+            }
+
+            // パスワードが一致するとき行を削除
+            if ($passward == $del_passward) {
+                $sql = 'DELETE FROM tbbbs WHERE id=:del_num ';
+                $stmt = $pdo->prepare($sql);                  // ←差し替えるパラメータを含めて記述したSQLを準備し、
+                $stmt->bindParam(':del_num', $del_num, PDO::PARAM_INT); // ←その差し替えるパラメータの値を指定してから、
+                $stmt->execute();
+            }
+        }
+    }
+
+    // 記事編集処理
+    if (isset($_POST["edit_num"]) && isset($_POST["edit_passward"])) {
+        if ((!empty($_POST["edit_num"])) && (!empty($_POST["edit_passward"]))) {
+            // フォームから値の読み込み
+            $edit_num = $_POST["edit_num"];
+            $edit_passward = $_POST["edit_passward"];
+
+            // idが$edit_numのパスワードを抽出
+            $sql = 'SELECT * FROM tbbbs WHERE id=:edit_num';
+            $stmt = $pdo->prepare($sql);                  // ←差し替えるパラメータを含めて記述したSQLを準備し、
+            $stmt->bindParam(':edit_num', $edit_num, PDO::PARAM_INT); // ←その差し替えるパラメータの値を指定してから、
+            $stmt->execute();
+            $results = $stmt->fetchAll();
+            foreach ($results as $row) {
+                $passward =  $row['passward'];
+            }
+            if ($passward == $edit_passward) {
+                $editid = $row['id'];
+                $editname = $row['name'];
+                $editcomment = $row['comment'];
+                $editpassward = $row['passward'];
+            }
+        }
+    }
+    ?>
+    <h2>テーマ:ひとこと</h2>
+    <form method="POST" action="">
+        <input type="name" name="name" value="<?php if (!empty($editname)) {
+                                                    echo $editname;
+                                                } else {
+                                                    echo "名前";
+                                                } ?>"><br>
+        <input type="comment" name="comment" value="<?php if (!empty($editcomment)) {
+                                                        echo $editcomment;
+                                                    } else {
+                                                        echo "コメント";
+                                                    } ?>"><br>
+        <input type="passward" name="passward" value="<?php if (!empty($editpassward)) {
+                                                            echo $editpassward;
+                                                        } else {
+                                                            echo "パスワード";
+                                                        } ?>"><br>
+        <input type="hidden" name="id" value="<?php if (!empty($editid)) {
+                                                    echo $editid;
+                                                } ?>"><br>
+        <input type="submit" name="submit" value="送信">
+    </form>
+    <form method="POST" action="">
+        <input type="del_num" name="del_num" value="削除番号指定"><br>
+        <input type="del_passward" name="del_passward" value="パスワード"><br>
+        <input type="submit" name="delete" value="削除">
+    </form>
+    <form method="POST" action="">
+        <input type="edit_num" name="edit_num" value="編集番号指定"><br>
+        <input type="edit_passward" name="edit_passward" value="パスワード"><br>
+        <input type="submit" name="edit" value="編集">
+    </form>
+
+    <?php
+    // テーブルtbtestからレコードを抽出
+    $sql = 'SELECT * FROM tbbbs';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+    foreach ($results as $row) {
+        //$rowの中にはテーブルのカラム名が入る
+        echo $row['id'] . ',';
+        echo $row['name'] . ',';
+        echo $row['comment'] . ',';
+        // echo $row['passward'] . ',';
+        echo $row['created_at'] . '<br>';
+        echo "<hr>";
+    }
+    ?>
+</body>
+
+</html>
